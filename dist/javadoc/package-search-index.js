20d4aa1482f9d9cd1ead5ebb90b3e81030083527
Jordan Medina Ortiz <120710792+JordanMedinaOrtiz@users.noreply.github.com>
2024-05-20 16:15:22 -0600

    Add files via upload

---
+++ dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"archivos"},{"l":"basededatos"},{"l":"gui"},{"l":"gui.clases"},{"l":"gui.drawer"},{"l":"gui.mapa"},{"l":"raven.tabbed"},{"l":"serviciodemensajeria"},{"l":"tabbed"}];updateSearchResults();
